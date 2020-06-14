7a668e0a06ea0622abecd569e0185d425bf0e1e8
roseline124 <guseod24@gmail.com>
2020-06-14 18:35:15 +0900

    bubblewrap build

---
+++ app/build/generated/source/buildConfig/release/app/vercel/roseline124_next_pwa_boilerplate_oq3l/twa/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package app.vercel.roseline124_next_pwa_boilerplate_oq3l.twa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "app.vercel.roseline124_next_pwa_boilerplate_oq3l.twa";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1";
+}
